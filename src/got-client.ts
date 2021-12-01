8c268d02c25f4be788dbe1ce0e1a683d9600bbc8
Caesarovich <cesar@caesarovich.xyz>
2021-12-01 22:07:30 +0100

    Fix retrying mechanism for status 202

--- src/got-client.ts
+++ src/got-client.ts
@@ -11,17 +11,15 @@ const gotClient = got.extend({
 	prefixUrl: 'https://analyze.intezer.com/api/v2-0/',
 	retry: {
 		limit: 5,
-		calculateDelay: ({ attemptCount }) => {
-			if (attemptCount > gotClient.defaults.options.retry.limit) return 0;
-			return 2 ** (attemptCount - 1) * 1000;
-		},
 		statusCodes: [202, 408, 429, 500, 502, 503, 504, 521, 522, 524],
 	},
 	hooks: {
 		afterResponse: [
-			(response, retryWithMergedOptions) => {
+			(response) => {
 				if (response.statusCode === 202) {
-					return retryWithMergedOptions(gotClient.defaults.options);
+					const err = new Error();
+					err.name = 'RetryError';
+					throw err;
 				}
 				return response;
 			},
