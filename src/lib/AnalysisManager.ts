0b2a45e61b7d54fe97d5ca6b9eea442cc329ffc3
Caesarovich <cesar@caesarovich.xyz>
2021-12-18 22:48:50 +0100

    Added fetchFile and getFile

--- src/lib/AnalysisManager.ts
+++ src/lib/AnalysisManager.ts
@@ -14,10 +14,31 @@ export class AnalysisManager extends CachedManager<String, Analysis> {
 	async fetch(id: string, options?: FetchOptions): Promise<Analysis> {
 		const analysis = new Analysis(this.client, await this.client.raw.getAnalysis(id));
 
-		if (options?.shouldCache) this.cache.set(analysis.id, analysis);
+		if (options?.shouldCache) {
+			this.cache.set(analysis.id, analysis);
+			this.cache.set(analysis.sha256, analysis);
+		}
 
-		return new Analysis(this.client, await this.client.raw.getAnalysis(id));
+		return analysis;
 	}
+
+	/**
+	 * Fetch an Analysis from the API and cache it **using the file's SHA256/SHA1/MD5 Hash**.
+	 *
+	 * @param hash The file's SHA256/SHA1/MD5 Hash.
+	 * @param options
+	 */
+	async fetchFile(hash: string, options?: FetchOptions): Promise<Analysis> {
+		const analysis = new Analysis(this.client, await this.client.raw.getFile(hash));
+
+		if (options?.shouldCache) {
+			this.cache.set(analysis.id, analysis);
+			this.cache.set(analysis.sha256, analysis);
+		}
+
+		return analysis;
+	}
+
 	/**
 	 * Get an analysis either from the cache or by fetching it.
 	 * It is recommended for performance reasons to use this method instead of {@link AnalysisManager.fetch **.fetch()**}.
@@ -30,6 +51,20 @@ export class AnalysisManager extends CachedManager<String, Analysis> {
 		const skipCache = options?.skipCache ?? this.client.options.shouldCache;
 
 		return cached && !skipCache ? cached : await this.fetch(id, options);
+	}
+
+	/**
+	 * Get an analysis either from the cache or by fetching it **using the file's SHA256/SHA1/MD5 Hash**.
+	 * It is recommended for performance reasons to use this method instead of {@link AnalysisManager.fetchFile **.fetchFile()**}.
+	 *
+	 * @param hash The file's SHA256/SHA1/MD5 Hash.
+	 * @param options
+	 */
+	async getFile(hash: string, options?: GetOptions): Promise<Analysis> {
+		const cached = this.cache.get(hash);
+		const skipCache = options?.skipCache ?? this.client.options.shouldCache;
+
+		return cached && !skipCache ? cached : await this.fetchFile(hash, options);
 	}
 
 	constructor(client: Client) {
