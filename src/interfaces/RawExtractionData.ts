3a7dc1d261196fb74287c2639a25cd7d545962fc
Caesarovich <cesar@caesarovich.xyz>
2021-12-19 07:19:54 +0100

    Add TODO

--- src/interfaces/RawExtractionData.ts
+++ src/interfaces/RawExtractionData.ts
@@ -1,6 +1,8 @@
 /**
  * This optional property is not relevant for the **root** file.
  * This field provides additional information about the Dynamic Execution or Static Extraction process.
+ *
+ * TODO: Find out an exact documentation about this data
  */
 export interface RawExtractionData {
 	collected_from: 'memory' | 'disk' | string;
